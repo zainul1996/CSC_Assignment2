fbe5ddb7056feb482cc53359dec2526cda629434
Zainul <Zainul1996@gmail.com>
2016-07-20 19:46:27 +0800

    Proxy Global Weather Service
    Added a reference service to mr ji's globalweatherservice asmx.
    Made a restful API controller to call the soap service.

---
+++ CSC_Assignment2/Controllers/WeatherController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Http;
+using CSC_Assignment2.GlobalWeatherService;
+using CSC_Assignment2.Models;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace CSC_Assignment2.Controllers
+{
+    [AllowAnonymous]
+    public class WeatherController : ApiController
+    {
+        //[HttpPost]
+        //[Route("api/v1/GetWeatherByLocation")]
+        //public IHttpActionResult GetWeatherByLocation([FromBody] dynamic geo)
+        //{
+        //    double lat = double.Parse((String)geo.lat);
+        //    double lng = double.Parse((String)geo.lng);
+        //    return Ok(GeonamesWeather.GetWeatherByLocation(lat,lng));
+
+
+        //}
+
+        //[HttpPost]
+        //[Route("api/v1/GetWeatherByLocationXML")]
+        //public IHttpActionResult GetWeatherByLocationXML([FromBody] dynamic geo)
+        //{
+        //    double lat = double.Parse((String)geo.lat);
+        //    double lng = double.Parse((String)geo.lng);
+        //    return Ok(GeonamesWeather.GetWeatherByLocationXML(lat, lng));
+
+
+        //}
+
+        [HttpGet]
+        [Route("api/v1/GetWeatherByCity/{cityName}/{countryName}")]
+        public IHttpActionResult GetWeatherByCity(string cityName, string countryName)
+        {
+            GlobalWeatherSoapClient client = new GlobalWeatherSoapClient();
+            string xml = client.GetWeather(cityName, countryName);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(CurrentWeather));
+            CurrentWeather currentWeather;
+            using (TextReader reader = new StringReader(xml))
+            {
+                currentWeather = (CurrentWeather)serializer.Deserialize(reader);
+            }
+
+            return Json(currentWeather);
+        }
+    }
+}
---
+++ CSC_Assignment2/Models/CurrentWeather.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CSC_Assignment2.Models
+{
+    [Serializable]
+    public class CurrentWeather
+    {
+        [XmlElement("Location")]
+        public string Location { get; set; }
+        [XmlElement("Time")]
+        public string Time { get; set; }
+        [XmlElement("Wind")]
+        public string Wind { get; set; }
+        [XmlElement("Visibility")]
+        public string Visibility { get; set; }
+        [XmlElement("SkyConditions")]
+        public string SkyConditions { get; set; }
+        [XmlElement("Temperature")]
+        public string Temperature { get; set; }
+        [XmlElement("DewPoint")]
+        public string DewPoint { get; set; }
+        [XmlElement("RelativeHumidity")]
+        public string RelativeHumidity { get; set; }
+        [XmlElement("Pressure")]
+        public string Pressure { get; set; }
+        [XmlElement("Status")]
+        public string Status { get; set; }
+
+    }
+}
